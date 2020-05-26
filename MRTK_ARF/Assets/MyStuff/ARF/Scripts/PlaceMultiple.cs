4b4679fe7039972c55060ffbf9bfcfb3e43d1611
Jatin Pawar <43639139+Jatin-Pawar@users.noreply.github.com>
2020-05-26 09:56:00 +0530

    first commit

---
+++ MRTK_ARF/Assets/MyStuff/ARF/Scripts/PlaceMultiple.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceMultiple : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    private GameObject[] placedPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject[] PlacedPrefab
+    {
+        get { return placedPrefab; }
+        set { placedPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedObject { get; private set; }
+
+    /// <summary>
+    /// Invoked whenever an object is placed in on a plane.
+    /// </summary>
+    public static event Action onPlacedObject;
+
+    ARRaycastManager m_RaycastManager;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+    int i = 0;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+                {
+                    i++;
+                    Pose hitPose = s_Hits[0].pose;
+
+                    spawnedObject = Instantiate(placedPrefab[i%placedPrefab.Length], hitPose.position, hitPose.rotation);
+
+                    if (onPlacedObject != null)
+                    {
+                        onPlacedObject();
+                    }
+                }
+            }
+        }
+    }
+}
